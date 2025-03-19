52c923e8cb57b7746e70eff8f23d5ad79f9ca8d2
LucasDellaMaestra <lucas.casluu33@gmail.com>
2025-03-19 17:00:44 +0000

    ejercicio analisis

--- src/app.py
+++ src/app.py
@@ -1,4 +1,19 @@
-from utils import db_connect
-engine = db_connect()
 
 # your code here
+import pandas as pd
+import os
+
+# URL del CSV
+url = "https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv"
+
+# Leer CSV desde la URL
+df = pd.read_csv(url)
+
+
+# Guardar archivo CSV crudo en 'data/raw'
+df.to_csv('data/raw/AB_NYC_2019.csv', index=False)
+
+print("✅ Datos crudos guardados en 'data/raw/AB_NYC_2019.csv'")
+
+# Obtener dimensiones
+df.shape
